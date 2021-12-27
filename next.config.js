bf59c307ce584cc6f61d6e3f1182b56231d9782a
healer1064 <healer1064729@gmail.com>
2021-12-27 16:15:14 -0300

    auth-header

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.discordapp.com', "static-cdn.jtvnw.net"]
+  },
+  env: {
+    HEADER_SECRET: process.env.HEADER_SECRET
   }
 }
